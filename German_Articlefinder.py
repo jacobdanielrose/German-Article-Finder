1f70d0f261bbd7237ca837b946a885a92eed47e9
= <=>
2022-02-15 12:56:32 +0100

    Initialize application

---
+++ German_Articlefinder.py
@@ -0,0 +1,7 @@
+print("Please enter a german noun:")
+
+wordInput = input()
+
+FeminineEndings = ["ung", "heit", "keit", "schaft", "ik", "ion"]
+NeuterEndings = ["nis", "tum", "lein", "chen", "ment"]
+MasculineEndings = ["ling", "ich", "ig"]
